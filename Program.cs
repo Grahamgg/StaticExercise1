de30bdd8693f4f261da84ed2cd5926402eda337d
Graham Galloway <Grahamgalloway0@gmail.com>
2022-10-04 11:55:31 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            {
+                Console.WriteLine($" celsius {celsius}");
+            }
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+            {
+                Console.WriteLine($" fahrenheit {fahrenheit}");
+            }
+
+        }
+    }
+}
